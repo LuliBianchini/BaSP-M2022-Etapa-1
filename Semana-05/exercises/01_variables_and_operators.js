1e727fb476d3da9050837c60f67ef410a523cf9f
lucia bianchini <lulibianchini97@gmail.com>
2022-04-15 13:28:28 -0300

    BaSP-W05-2022: ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+console.log ('~ Variables and Operators ~')
+/* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en
+una 3er variable.*/
+
+console.log ('-Exercise 1.a:')
+var firstNumber = 25;
+var secondNumber = 75;
+var result = firstNumber + secondNumber;
+console.log (result)
+
+/* b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+console.log ('-Exercise 1.b:')
+var firstName = 'Lucía ';
+var surname = 'Bianchini';
+var completeName = firstName + surname 
+console.log (completeName)
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el
+resultado de la suma en una 3er variable (utilizar length).*/
+
+console.log ('-Exercise 1.c:')
+var firstString = ('strawberry fields')
+var secondString = ('yellow submarine')
+var totalCharacters = firstString + secondString 
+console.log (totalCharacters.length)
+
